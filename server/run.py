fd33515236f2fcb5fa4361d0c888e868c39c6272
Michele Filannino <filannino.m@gmail.com>
2015-04-18 04:37:56 +0100

    thumbs and gps coords

--- server/run.py
+++ server/run.py
@@ -11,19 +11,21 @@
 #
 #   For details, just contact us! ;)
 
+from PIL import Image
 import os
 
-from flask import Flask, render_template, session, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request
 from werkzeug import secure_filename
 
 
-UPLOAD_FOLDER = os.path.abspath('../pictures')
-ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
+UPL_FOLDER = os.path.abspath('../pictures')
+ALLOWED_EXTENSIONS = set(['.jpg', '.jpeg', '.png'])
+
 
 def main():
     # Web server initialisation
     app = Flask("boo")
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    app.config['UPL_FOLDER'] = UPL_FOLDER
     app.debug = True
     app.secret_key = 'A0Zr85j/3yX-R~XFH!jmN]31X/,?RT'
 
@@ -43,9 +45,10 @@ def main():
 
     @app.route('/upload', methods=['POST'])
     def upload():
-        shot = request.files['file']
+        shot = request.files['shot_file']
         if not shot:
             return 'Picture unreadable!'
+        image = Image.open(shot)
 
         # check for extension
         shot_name, shot_extension = os.path.splitext(shot.filename)
@@ -53,14 +56,30 @@ def main():
             return '.{} extension not allowed!'.format(shot_extension.upper())
 
         # check for GPS coordinates
+        gps_dec_tag = 34853
+        gps_latitude, gps_longitude = None, None
+        try:
+            exif_gps = image._getexif()[gps_dec_tag]
+            gps_latitude = (exif_gps[1], exif_gps[2])
+            gps_longitude = (exif_gps[3], exif_gps[4])
+            print 'Latitude:', gps_latitude
+            print 'Longitude:', gps_longitude
+        except Exception:
+            return 'No GPS coordinates found!'
 
         # store the picture
         shot_name = secure_filename(shot.filename)
-        shot.save(os.path.join(app.config['UPLOAD_FOLDER'], shot_name))
+        shot.save(os.path.join(app.config['UPL_FOLDER'], shot_name))
 
         # store its coordinates
 
         # store its thumbnail
+        scaling = image.size[1] / 100
+        thumb_size = (image.size[0] / scaling, 100)
+        image.thumbnail(thumb_size, Image.ANTIALIAS)
+        image.save(os.path.join(app.config['UPL_FOLDER'], 'thumbnails',
+                                shot.filename),
+                   "JPEG")
 
         return redirect(url_for('thanks'))
 
